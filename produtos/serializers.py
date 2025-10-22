5382728fb07d92db539f6d8000148a46f5c2e2e6
henrique.furtado47 <henrique.furtadoifc@gmail.com>
2025-10-22 09:48:32 -0300

    feat(api): adiciona TipoSerializer e atualiza ProdutoSerializer com nova relação

---
+++ produtos/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import Categoria, Produto, Tipo
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ['id', 'nome', 'descricao']
+
+class TipoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tipo
+        fields = ['id', 'nome', 'descricao']
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    categoria = CategoriaSerializer()
+    tipo = TipoSerializer()
+
+    class Meta:
+        model = Produto
+        fields = [
+            'id', 'nome', 'descricao', 'preco', 'categoria', 'tipo', 'imagem',
+            'dano', 'defesa', 'durabilidade', 'slot_custo', 'efeito',
+        ]
