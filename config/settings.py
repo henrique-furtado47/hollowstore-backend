2ff000c2a4ba0e630759839b114214a981ea34dc
henrique.furtado47 <henrique.furtadoifc@gmail.com>
2025-10-22 10:37:06 -0300

    feat(settings): adicionado cors

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'produtos',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
