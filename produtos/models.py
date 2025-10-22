6f0cc4528526fef408c3c3289ebbf9eded5b750b
henrique.furtado47 <henrique.furtadoifc@gmail.com>
2025-10-22 09:48:32 -0300

    feat(models): adiciona modelo Tipo e ajusta relação com Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+    descricao = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return self.nome
+
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+    descricao = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Tipos"
+
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)    
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.SET_NULL, null=True, related_name='produtos'
+    )
+    tipo = models.ForeignKey(
+        Tipo, on_delete=models.SET_NULL, null=True, related_name='produtos'
+    )
+    imagem = models.CharField(max_length=255, blank=True, null=True)
+    # Campos opcionais
+    dano = models.IntegerField(blank=True, null=True)
+    defesa = models.IntegerField(blank=True, null=True)
+    durabilidade = models.IntegerField(blank=True, null=True)
+    slot_custo = models.PositiveIntegerField(blank=True, null=True)
+    efeito = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
