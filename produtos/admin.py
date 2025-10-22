5fc4e9baea2cf76be64aa766fd36773666abf3a4
henrique.furtado47 <henrique.furtadoifc@gmail.com>
2025-10-22 09:48:32 -0300

    chore(admin): registra Tipo e ajusta exibição de Produto e Categoria

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Categoria, Produto, Tipo
+
 # Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+    search_fields = ('nome',)
+
+@admin.register(Tipo)
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+    search_fields = ('nome',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'categoria', 'tipo', 'imagem')
+    list_filter = ('categoria', 'tipo')
+    search_fields = ('nome', 'descricao')
+    ordering = ('nome',)
