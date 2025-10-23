cce13219b79507374c0b0bb3b9ab906d67ce0083
henrique.furtado47 <henrique.furtadoifc@gmail.com>
2025-10-23 08:10:42 -0300

    docs(swagger): adicionado documentação de api swagger

--- produtos/urls.py
+++ produtos/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CategoriaViewSet, ProdutoViewSet, TipoViewSet
 
+
 router = DefaultRouter()
 router.register(r'produtos', ProdutoViewSet)
 router.register(r'categorias', CategoriaViewSet)
