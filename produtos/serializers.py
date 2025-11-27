b3da14dd7f2221f4384ca8f864d03b11eb252b33
PauloK3tchup <paulocesarifc@gmail.com>
2025-11-27 08:14:22 -0300

    feat: poggers

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -32,8 +32,22 @@ class ItemPedidoSerializer(serializers.ModelSerializer):
         fields = ['produto', 'nome_produto', 'quantidade', 'preco_unitario',"imagem"]
 
 class PedidoSerializer(serializers.ModelSerializer):
-    itens = ItemPedidoSerializer(many=True, read_only=True)
+    # Removido o read_only=True para permitir escrita
+    itens = ItemPedidoSerializer(many=True)
 
     class Meta:
         model = Pedido
         fields = ['id', 'status', 'data_criacao', 'total', 'itens']
+
+    def create(self, validated_data):
+        # 1. Separa os dados dos itens dos dados do pedido principal
+        itens_data = validated_data.pop('itens')
+        
+        # 2. Cria o Pedido (o usuário já vem no validated_data ou via perform_create da view)
+        pedido = Pedido.objects.create(**validated_data)
+        
+        # 3. Cria cada ItemPedido vinculado a esse novo pedido
+        for item_data in itens_data:
+            ItemPedido.objects.create(pedido=pedido, **item_data)
+            
+        return pedido
